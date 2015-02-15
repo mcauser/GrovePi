863d77d7a200683a0ba395eaf2841c1f5f496f5c
Mike Causer <mcauser@gmail.com>
2015-02-15 23:07:25 +1100

    Documentation

--- Software/Python/grove_dht_pro.py
+++ Software/Python/grove_dht_pro.py
@@ -7,10 +7,31 @@ import grovepi
 # SIG,NC,VCC,GND
 sensor = 4
 
+# There are 3 supported sensor modules:
+#module = 0  # DHT11 blue
+module = 1   # DHT22 white / AM2302 wired version of DHT22 in larger plastic shell
+#module = 2  # DHT21 black / AM2301 wired version of DHT21 in larger plastic shell
+
+# DHT11:
+# http://www.seeedstudio.com/wiki/Grove_-_Temperature_and_Humidity_Sensor
+# http://www.adafruit.com/products/386
+
+# DHT22:
+# http://www.seeedstudio.com/wiki/Grove_-_Temperature_and_Humidity_Sensor_Pro
+# http://www.adafruit.com/products/385
+
+# AM2302:
+# http://www.adafruit.com/product/393
+
+# Learn more about the DHT modules:
+# https://learn.adafruit.com/dht
+
 while True:
     try:
-        [temp,humidity] = grovepi.dht(sensor,1)
-        print "temp =", temp, " humidity =", humidity
+        [temp,humidity] = grovepi.dht(sensor,module)
+        print "temperature =", temp, "*C, humidity =", humidity, "%"
 
+    except KeyboardInterrupt:
+        break
     except IOError:
         print "Error"
